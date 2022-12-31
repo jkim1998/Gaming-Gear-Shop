9d7036d033add123b35e6b9dacdee1852c6a2c59
Jonathan Kim <jonathankim980@gmail.com>
2022-12-31 11:21:17 -0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { client } from '../lib/client';
-import { Product, FooterBanner, HeroBanner } from '../Components';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
